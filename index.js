2aa357deb66d3c93b7641328b342d0bc28b4c142
agentginger <coperu932@gmail.com>
2024-01-23 08:55:23 +0000

    Added accented é. Added to ToDo list.

--- index.js
+++ index.js
@@ -20,13 +20,14 @@ shoppingList.push(" and ice cream");
 const listAsString = shoppingList.join();
 
 const shoppingListPre = "I need to pickup ";
-
-const shoppingListPost = " from the supermache";
+const shoppingListPost = " from the supermarch\xE9.";
 
 console.log(`${shoppingListPre}${listAsString}${shoppingListPost}`);
+
 
 // console.log(`${shoppingListPre} shoppingList.join() ${shoppingListPost}`);
 // Above deliberatly doesnt work but Id like to know why.
 
 //ToDo
+//Create alt shopping list whereby shoppingListPre and shoppingListPost are added using array methods push and unshift
 //Work out how to add an item to the shopping list at any index.
